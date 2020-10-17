9b1ceb2c54d1a9691b69c28b233686ff2b1d278b
hazardinho <73017529+hazardinho@users.noreply.github.com>
2020-10-17 12:18:57 +0300

    Revert "Update bot.py"
    This reverts commit 2a9135408f547f774bd877c0c36ec7385eab44db.

--- PythonViewBot/bot.py
+++ PythonViewBot/bot.py
@@ -5,10 +5,10 @@ from selenium import webdriver;
 Timer = 120
 
 #youtube link
-link = 'https://youtu.be/zkBWZYtfCLc'
+link = 'https://www.youtube.com/watch?v=hW_WFUs3hfQ'
 
 #number of views
-views = 100
+views = 20
 
 driver = webdriver.Chrome()
 driver.get(link)
